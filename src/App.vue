697adb7d18270c42168cf7597038190358442dcc
WMXPY <wm@wmpcxpy.com>
2017-07-10 20:21:39 -0500

    nothing changeds

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,7 @@
       <dog-block icon="car" info="123">
         <dog-cell left="aoc">testatsat</dog-cell>
       </dog-block>
-      <dog-block icon="plus" info="123">
-        <dog-cell left="aoc">testatsat</dog-cell>
-        <dog-cell left="aoc">testatsat</dog-cell>
-      </dog-block>
-      <dog-block icon="car" info="fafda">
-        123
-      </dog-block>
-      <dog-block icon="star" info="123">
-        <dog-cell left="aoc">testatsat</dog-cell>
-      </dog-block>
-      <dog-block icon="user" info="123">
-        123
-      </dog-block>
-      <dog-block icon="car" info="123">
-        123
-      </dog-block>
       <dog-button v-on:click="test" icon="car" size="large" color="red">123</dog-button>
-      <dog-button v-on:click="test" icon="car" size="large" color="orange">123</dog-button>
-      <dog-button v-on:click="test" icon="car" size="large" color="green">123</dog-button>
-      <dog-button v-on:click="test" icon="car" size="large" color="blue">123</dog-button>
-      <dog-button v-on:click="test" icon="car" size="large" color="black">123</dog-button>
-      <dog-button v-on:click="test" icon="car" size="large" color="purple">123</dog-button>
     </dog-title>
   </div>
 </template>
