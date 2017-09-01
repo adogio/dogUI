15a4e88d36afbf3cfdb477eed5e35ba918cb4358
PCXPY <pc@wmpcxpy.com>
2017-08-31 19:07:47 -0500

    4.5.5

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Every dogui component can work individually without any other dogui component.
 
 ## Version
 
--   Version 4.5.4
+-   Version 4.5.5
 -   See [Angular version of dogui](https://github.com/adogio/dogui-angular)
 -   See [React version of dogui](https://github.com/adogio/dogui-react)
 
--- src/vue/out/pictureView.vue
+++ src/vue/out/pictureView.vue
@@ -1,118 +1,115 @@
 <template>
-    <div>
-        <div class="row" v-for="(row, indexl) in rows" v-if="small&&rows">
-            <div class="col-xs-4" v-for="(i, indexr) in row">
+    <div v-if="small&&rows">
+        <div class="row" v-for="(row, indexl) in rows" :key="indexl">
+            <div class="col-xs-4" v-for="(i, indexr) in row" :key="indexr">
                 <div class="box" @click="biggerClick(indexl, indexr)">
-                    <img :src="i">
+                    <img :src="i" :alt="alt?alt:'X'">
                 </div>
             </div>
         </div>
         <div v-if="!small">
             <div class="box" @click="smallClick">
-                <img :src="biggerone">
+                <img :src="biggerone" :alt="alt?alt:'X'">
             </div>
             <div class="displayText">
                 <slot></slot>
             </div>
-            <!-- <dog-button icon="download" color="purple" @click="download">
-                <slot></slot>
-            </dog-button> -->
         </div>
     </div>
 </template>
 
 <script>
-    import button from './button.vue';
-    export default {
-        data: function () {
-            return {
-                rows: null,
-                small: true,
-                left: 0,
-                top: 0
-            }
-        },
-        computed: {
-            biggerone: function () {
-                return this.rows[this.left][this.top];
-            }
-        },
-        mounted: function () {
-            this.rows = this.getrows();
-        },
-        name: "dog-view",
-        props: ['srcs'],
-        components: {
-            "dog-button": button
-        },
-        methods: {
-            download: function () {
-                // TODO
-                // let dataURI = this.rows[this.left][this.top];
-                // var byteString = atob(dataURI.split(',')[1]);
-                // var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
-                // var ab = new ArrayBuffer(byteString.length);
-                // var ia = new Uint8Array(ab);
-                // for (var i = 0; i < byteString.length; i++) {
-                //     ia[i] = byteString.charCodeAt(i);
-                // }
+import button from './button.vue';
+export default {
+    data: function() {
+        return {
+            rows: null,
+            small: true,
+            left: 0,
+            top: 0
+        }
+    },
+    computed: {
+        biggerone: function() {
+            return this.rows[this.left][this.top];
+        }
+    },
+    mounted: function() {
+        this.rows = this.getrows();
+    },
+    name: "dog-view",
+    props: ['srcs', 'alt'],
+    components: {
+        "dog-button": button
+    },
+    methods: {
+        download: function() {
+            // TODO
+            // let dataURI = this.rows[this.left][this.top];
+            // var byteString = atob(dataURI.split(',')[1]);
+            // var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+            // var ab = new ArrayBuffer(byteString.length);
+            // var ia = new Uint8Array(ab);
+            // for (var i = 0; i < byteString.length; i++) {
+            //     ia[i] = byteString.charCodeAt(i);
+            // }
 
-                // var bb = new Blob([ab]);
-                // const a = document.createElement('a');
-                // const url = window.URL.createObjectURL(bb);
-                // a.href = url;
-                // a.download = 'atthis_dl.jpg';
-                // a.click();
-                // window.URL.revokeObjectURL(url);
-            },
-            biggerClick: function (indexl, indexr) {
-                this.left = indexl;
-                this.top = indexr;
-                this.small = false;
-            },
-            smallClick: function () {
-                this.small = true;
-            },
-            getrows: function () {
-                let srcs = this.srcs;
-                let re = [];
-                let temp = [];
-                while (srcs.length != 0) {
-                    temp.push(srcs.shift());
-                    if (temp.length == 3 || srcs.length == 0) {
-                        re.push(temp);
-                        temp = [];
-                    }
+            // var bb = new Blob([ab]);
+            // const a = document.createElement('a');
+            // const url = window.URL.createObjectURL(bb);
+            // a.href = url;
+            // a.download = 'atthis_dl.jpg';
+            // a.click();
+            // window.URL.revokeObjectURL(url);
+        },
+        biggerClick: function(indexl, indexr) {
+            this.left = indexl;
+            this.top = indexr;
+            this.small = false;
+        },
+        smallClick: function() {
+            this.small = true;
+        },
+        getrows: function() {
+            let srcs = this.srcs;
+            let re = [];
+            let temp = [];
+            while (srcs.length != 0) {
+                temp.push(srcs.shift());
+                if (temp.length == 3 || srcs.length == 0) {
+                    re.push(temp);
+                    temp = [];
                 }
-                return re;
             }
+            return re;
         }
     }
+}
 </script>
 
 <style scoped>
-    .box {
-        width: 100%;
-        height: 100%;
-        text-align: center;
-    }
+.box {
+    width: 100%;
+    height: 100%;
+    text-align: center;
+}
 
-    div.displayText {
-        font-size: 15px;
-        text-align: center;
-        font-family: 'ubuntu';
-    }
+div.displayText {
+    font-size: 15px;
+    text-align: center;
+    font-family: 'ubuntu';
+}
 
-    .box img {
-        max-width: 100%;
-        max-height: 100%;
-        width: 100%;
-        height: auto;
-    }
+.box img {
+    max-width: 100%;
+    max-height: 100%;
+    width: 100%;
+    height: auto;
+}
 
-    div.col-xs-4,
-    div.row {
-        margin: 0px;
-        padding: 0px;
-    }
+div.col-xs-4,
+div.row {
+    margin: 0px;
+    padding: 0px;
+}
 </style>
